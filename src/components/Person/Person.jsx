e520169fb5129609d27579bbde05d0af8d5cb48c
Batuhan <mrkayahan66@gmail.com>
2024-02-15 15:28:38 +0300

    version

---
+++ src/components/Person/Person.jsx
@@ -0,0 +1,9 @@
+const Person=() => {
+    return(
+        <div>
+            <h1>Person2</h1>
+        </div>
+    );
+};
+
+export default Person;
---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const kisiler = [
+    {
+      isim: "Ahmet",
+      email: "ahmet@ornek.com",
+      numara: "555-555-5555",
+    },
+    {
+      isim: "Fatma",
+      email: "fatma@ornek.com",
+      numara: "555-555-5556",
+    },
+    {
+      isim: "Ayşe",
+      email: "ayse@ornek.com",
+      numara: "555-555-5557",
+    },
+  ];
